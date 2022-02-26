617e8afb0a5a4ef2ad3ec334ac9887df7a4646d5
bobdeng <bobdeng@163.com>
2022-02-26 16:10:58 +0800

    refactor

--- src/main/java/cn/bobdeng/base/rbac/ListUserController.java
+++ src/main/java/cn/bobdeng/base/rbac/ListUserController.java
@@ -1,27 +1,23 @@
 package cn.bobdeng.base.rbac;
 
-import cn.bobdeng.base.rbac.permission.PermissionSessionGetter;
 import cn.bobdeng.base.rbac.permission.SessionUser;
 import cn.bobdeng.base.user.Users;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 public class ListUserController {
-    private PermissionSessionGetter permissionSessionGetter;
+    private ListUserService listUserService;
 
-    public ListUserController(PermissionSessionGetter permissionSessionGetter) {
-        this.permissionSessionGetter = permissionSessionGetter;
+    public ListUserController(ListUserService listUserService) {
+        this.listUserService = listUserService;
     }
 
     @GetMapping("/rbac/users")
     public List<UserVO> listUser() {
-        SessionUser sessionUser = permissionSessionGetter.sessionUser().orElseThrow();
-        return Users.ofTenant(sessionUser.getTenantId()).list().stream()
-                .map(UserVO::new).collect(Collectors.toList());
+        return listUserService.listUser();
     }
 }
---
+++ src/main/java/cn/bobdeng/base/rbac/ListUserService.java
@@ -0,0 +1,25 @@
+package cn.bobdeng.base.rbac;
+
+import cn.bobdeng.base.rbac.permission.PermissionSessionGetter;
+import cn.bobdeng.base.rbac.permission.SessionUser;
+import cn.bobdeng.base.user.Users;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ListUserService {
+    public ListUserService(PermissionSessionGetter permissionSessionGetter) {
+        this.permissionSessionGetter = permissionSessionGetter;
+    }
+
+    private PermissionSessionGetter permissionSessionGetter;
+
+    public List<UserVO> listUser() {
+        SessionUser sessionUser = permissionSessionGetter.sessionUser().orElseThrow();
+        return Users.ofTenant(sessionUser.getTenantId()).list().stream()
+                .map(UserVO::new).collect(Collectors.toList());
+    }
+
+}
